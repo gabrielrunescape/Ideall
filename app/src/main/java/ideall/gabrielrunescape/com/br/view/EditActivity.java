414fb3953756da1e734e17e89e9aa721d3ec6133
GabrielRuneScape <gabrielfilipe@mail.ru>
2016-11-23 21:31:23 -0200

    Inserção do arquivo activity_edit.xml e alterações nas cores e nas strings

---
+++ app/src/main/java/ideall/gabrielrunescape/com/br/view/EditActivity.java
@@ -0,0 +1,13 @@
+package ideall.gabrielrunescape.com.br.view;
+
+import android.os.Bundle;
+import ideall.gabrielrunescape.com.br.R;
+import android.support.v7.app.AppCompatActivity;
+
+public class EditActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+}
