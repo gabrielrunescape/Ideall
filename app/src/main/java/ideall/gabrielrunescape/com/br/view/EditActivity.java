77473567eaf18cd32fe0dedb3d5b59eaddc42a0e
Gabriel Filipe <gabrielfilipe@mail.ru>
2016-11-25 00:18:25 -0200

    Substituidos os FloatingActionButton para MenuItems no layout e nas classes gerenciadoras na classe EditActivity

--- app/src/main/java/ideall/gabrielrunescape/com/br/view/EditActivity.java
+++ app/src/main/java/ideall/gabrielrunescape/com/br/view/EditActivity.java
@@ -1,27 +1,31 @@
 package ideall.gabrielrunescape.com.br.view;
 
 import android.os.Bundle;
-import android.view.View;
+import android.view.Menu;
 import android.widget.Toast;
+import android.view.MenuItem;
 import android.content.Intent;
 import android.widget.EditText;
 import ideall.gabrielrunescape.com.br.R;
+import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
 import ideall.gabrielrunescape.com.br.DAO.ProjectDAO;
 import ideall.gabrielrunescape.com.br.objects.Project;
-import android.support.design.widget.FloatingActionButton;
 
 public class EditActivity extends AppCompatActivity {
+    private MenuItem undo;
+    private MenuItem done;
     private EditText etBy;
     private EditText etName;
     private ProjectDAO project;
-    private FloatingActionButton btnSave;
-    private FloatingActionButton btnClose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_edit);
+        setSupportActionBar(toolbar);
 
         project = new ProjectDAO(this);
         project.open();
@@ -32,54 +36,11 @@ public class EditActivity extends AppCompatActivity {
         project.open();
         super.onResume();
 
-        final Intent intent = getIntent();
-
         etBy = (EditText) findViewById(R.id.etBy);
         etName = (EditText) findViewById(R.id.etName);
 
-        btnSave = (FloatingActionButton) findViewById(R.id.btnSave);
-        btnClose = (FloatingActionButton) findViewById(R.id.btnClose);
-
-        btnClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-
-                startActivity(intent);
-                finish();
-            }
-        });
-
-        btnSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String n = etName.getText().toString();
-                String b = etBy.getText().toString();
-
-                if (n.isEmpty() || b.isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "Existem campos vazios!", Toast.LENGTH_SHORT).show();
-                } else {
-                    Project p = new Project();
-                    p.setName(n);
-                    p.setAuthor(b);
-
-                    if (intent.getSerializableExtra("Project") == null) {
-                        project.create(p);
-                    } else {
-                        p.setID(((Project) intent.getSerializableExtra("Project")).getID());
-                        project.update(p);
-                    }
-
-                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-
-                    startActivity(intent);
-                    finish();
-                }
-            }
-        });
-
-        if (intent.getSerializableExtra("Project") != null) {
-            Project p = (Project) intent.getSerializableExtra("Project");
+        if (getIntent().getSerializableExtra("Project") != null) {
+            Project p = (Project) getIntent().getSerializableExtra("Project");
 
             etBy.setText(p.getAuthor());
             etName.setText(p.getName());
@@ -90,5 +51,58 @@ public class EditActivity extends AppCompatActivity {
     protected void onPause() {
         project.close();
         super.onPause();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_edit, menu);
+
+        undo = menu.findItem(R.id.item_undo);
+        done = menu.findItem(R.id.item_done);
+
+        undo.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+
+                startActivity(intent);
+                finish();
+
+                return true;
+            }
+        });
+
+        done.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                String n = etName.getText().toString();
+                String b = etBy.getText().toString();
+
+                if (n.isEmpty() || b.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Existem campos vazios!", Toast.LENGTH_SHORT).show();
+
+                    return false;
+                } else {
+                    Project p = new Project();
+                    p.setName(n);
+                    p.setAuthor(b);
+
+                    if (getIntent().getSerializableExtra("Project") == null) {
+                        project.create(p);
+                    } else {
+                        p.setID(((Project) getIntent().getSerializableExtra("Project")).getID());
+                        project.update(p);
+                    }
+
+                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+
+                    startActivity(intent);
+                    finish();
+
+                    return true;
+                }
+            }
+        });
+
+        return true;
     }
 }
